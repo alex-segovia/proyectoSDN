1d7f73f0e840c7464526e7af818820d57cdc243f
AlexZ_19 <a20213849@pucp.edu.pe>
2024-11-25 19:59:34 -0500

    Primer commit

---
+++ src/test/java/com/example/proyectosdn/ProyectoSdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectosdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSdnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
