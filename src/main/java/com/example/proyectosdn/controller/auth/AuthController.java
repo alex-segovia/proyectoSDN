fb3d17df8f0b2984be23856c65b45f4e637ce01e
AlexZ_19 <a20213849@pucp.edu.pe>
2024-11-28 00:38:35 -0500

    vistas de login y registro

--- src/main/java/com/example/proyectosdn/controller/auth/AuthController.java
+++ src/main/java/com/example/proyectosdn/controller/auth/AuthController.java
@@ -1,24 +1,42 @@
 package com.example.proyectosdn.controller.auth;
 
+import com.example.proyectosdn.entity.Usuario;
+import com.example.proyectosdn.repository.UsuarioRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/auth")
+@RequestMapping("/autenticacion")
 @Slf4j
 public class AuthController {
-    @GetMapping("/iniciar-sesion")
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/")
     public String mostrarLogin(Model model,
                                @RequestParam(required = false) String logout,
                                HttpSession session) {
-
         return "login";
+    }
 
+    @PostMapping("/registro")
+    public String registrarUsuario(@Valid Usuario usuario,
+                                   BindingResult bindingResult,
+                                   Model model,
+                                   RedirectAttributes redirectAttributes) {
+
+        return "";
     }
 }
