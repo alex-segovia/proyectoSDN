9d151d0a707ccab0878b14088fe6ff1cc34cb2b4
AlexZ_19 <a20213849@pucp.edu.pe>
2024-12-20 11:02:36 -0500

    asfafs

--- src/main/java/com/example/proyectosdn/repository/PuertoPorServicioRepository.java
+++ src/main/java/com/example/proyectosdn/repository/PuertoPorServicioRepository.java
@@ -2,7 +2,9 @@ package com.example.proyectosdn.repository;
 
 import com.example.proyectosdn.entity.PuertoPorServicio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,5 +13,7 @@ public interface PuertoPorServicioRepository extends JpaRepository<PuertoPorServ
     List<PuertoPorServicio> findByServicioId(Integer servicioId);
     List<PuertoPorServicio> findByPuertoId(Integer puertoId);
 
+    @Modifying
+    @Transactional
     void deleteByServicioId(Integer servicioId);
 }
